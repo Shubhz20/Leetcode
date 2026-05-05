5c8e31a843e283d5d2033237ad749c52c389e12a
Harshit Agrawal <harshit.agrawal@adypu.edu.in>
2026-05-05 13:59:40 +0530

    Time: 0 ms (100%), Space: 19.5 MB (8.1%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        if not head or not head.next or k == 0:
+            return head
+        
+        # Step 1: find length and tail
+        n = 1
+        tail = head
+        
+        while tail.next:
+            tail = tail.next
+            n += 1
+        
+        # Step 2: optimize k
+        k %= n
+        if k == 0:
+            return head
+        
+        # Step 3: make circular
+        tail.next = head
+        
+        # Step 4: find new tail
+        steps = n - k
+        new_tail = head
+        
+        for _ in range(steps - 1):
+            new_tail = new_tail.next
+        
+        # Step 5: new head
+        new_head = new_tail.next
+        
+        # Step 6: break cycle
+        new_tail.next = None
+        
+        return new_head
